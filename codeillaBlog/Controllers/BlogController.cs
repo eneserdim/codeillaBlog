a19930d3180358093be6f024387cd04fbe0af9d2
Enes Erdim <eneserdim1@gmail.com>
2021-10-27 19:58:25 +0300

    Add project files.

---
+++ codeillaBlog/Controllers/BlogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace codeillaBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Kategori()
+        {
+            return View();
+        }
+        public ActionResult Populer()
+        {
+            return View();
+        }
+        public ActionResult Etiket()
+        {
+            return View();
+        }
+    }
+}
